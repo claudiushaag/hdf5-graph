ae175f54c80328ec5389d5f0268319c06f257fa9
Claudius Haag <claudius.haag@mib.uni-stuttgart.de>
2024-08-21 17:05:29 +0200

    Get the creation and saving of values going

--- README.md
+++ README.md
@@ -3,3 +3,181 @@
 [![Pixi Badge](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/prefix-dev/pixi/main/assets/badge/v0.json)](https://pixi.sh)
 <!-- [![Test](https://github.tik.uni-stuttgart.de/DAE/molten/actions/workflows/pytest.yml/badge.svg?branch=main&event=push)](https://github.tik.uni-stuttgart.de/DAE/molten/actions/workflows/pytest.yml)
 [![Docs](https://github.tik.uni-stuttgart.de/DAE/molten/actions/workflows/docs.yml/badge.svg?branch=main&event=push)](https://github.tik.uni-stuttgart.de/DAE/molten/actions/workflows/docs.yml) -->
+
+
+## What are we going for?
+
+- circle is node
+- round edges is node with neo4j datatype
+- rectangle is property
+
+```mermaid
+mindmap
+  root((mindmap))
+    ((grp1))
+        ((BF_curves))
+
+        ((feap_variables))
+            id2(ca)
+            (cb)
+            (dt)
+        ((macrofail))
+            [hdf5_path]
+    ((grp2))
+        ((BF_curves))
+        ((feap_variables))
+            id2(ca)
+            (cb)
+            (dt)
+        ((macrofail))
+            [hdf5_path]
+```
+
+### Example Cypher Queries
+
+Return all datapoints hdf5 paths where ca=0.3
+```Cypher
+MATCH (d:Dataset{name:"datapoint"})-[]->(e:Experiment)-[]->(c:Dataset{name:"ca",value:0.3})
+RETURN d.hdf5_path
+```
+Return all datapoints hdf5 paths where ca=0.3 and which were identified as the first curve
+```Cypher
+MATCH (d:Dataset{name:"datapoint"})-[]->(e:Experiment)-[]->(c:Dataset{name:"ca",value:0.3})
+WHERE d.hdf5_path CONTAINS '\Curve_0\'
+RETURN d.hdf5_path
+```
+
+### Rules:
+
+- top groups are experiments
+
+```mermaid
+erDiagram
+    Experiment ||--o{ Dataset : holds
+    Experiment ||--o{ Characteristic: holds
+    Experiment {
+        string hdf5_path
+        string name
+        string test
+    }
+    Dataset {
+        string hdf5_path
+    }
+    characteristic {
+        string hdf5_path
+    }
+```
+
+### H5-Example
+
+```xml
+HDF5 "data\CompleteData.h5" {
+GROUP "/" {
+   GROUP "202305190830" {
+      GROUP "BF_curves" {
+         GROUP "Curve_0" {
+            GROUP "Spline" {
+               ATTRIBUTE "Date" {
+                  DATATYPE  H5T_STRING {
+                     STRSIZE H5T_VARIABLE;
+                     STRPAD H5T_STR_NULLTERM;
+                     CSET H5T_CSET_UTF8;
+                     CTYPE H5T_C_S1;
+                  }
+                  DATASPACE  SCALAR
+                  DATA {
+                  (0): "2024/07/01 16:42:42"
+                  }
+               }
+               ATTRIBUTE "Info" {
+                  DATATYPE  H5T_STRING {
+                     STRSIZE H5T_VARIABLE;
+                     STRPAD H5T_STR_NULLTERM;
+                     CSET H5T_CSET_UTF8;
+                     CTYPE H5T_C_S1;
+                  }
+                  DATASPACE  SCALAR
+                  DATA {
+                  (0): "Spline-Object"
+                  }
+               }
+               ATTRIBUTE "Package" {
+                  DATATYPE  H5T_STRING {
+                     STRSIZE H5T_VARIABLE;
+                     STRPAD H5T_STR_NULLTERM;
+                     CSET H5T_CSET_UTF8;
+                     CTYPE H5T_C_S1;
+                  }
+                  DATASPACE  SCALAR
+                  DATA {
+                  (0): "Spline Package by Claudius Haag"
+                  }
+               }
+               ATTRIBUTE "Version" {
+                  DATATYPE  H5T_IEEE_F64LE
+                  DATASPACE  SCALAR
+                  DATA {
+                  (0): 0.6
+                  }
+               }
+               DATASET "C" {
+                  DATATYPE  H5T_IEEE_F64LE
+                  DATASPACE  SIMPLE { ( 116, 156 ) / ( 116, 156 ) }
+                  DATA {XXXXX}
+               }
+            }
+            DATASET "Spline_Interp" {
+               DATATYPE  H5T_IEEE_F64LE
+               DATASPACE  SIMPLE { ( 1000, 2 ) / ( 1000, 2 ) }
+               DATA {XXXX}
+            }
+            DATASET "datapoints" {
+               DATATYPE  H5T_IEEE_F64LE
+               DATASPACE  SIMPLE { ( 96, 2 ) / ( 96, 2 ) }
+               DATA {
+               (0,0): 5.8783e-17, -0.213193,
+               }
+            }
+        }
+        GROUP "Curve_1" {
+            GROUP "Spline" {
+               ATTRIBUTE "Date" {
+                  DATATYPE  H5T_STRING {
+                     STRSIZE H5T_VARIABLE;
+                     STRPAD H5T_STR_NULLTERM;
+                     CSET H5T_CSET_UTF8;
+                     CTYPE H5T_C_S1;
+                  }
+                  DATASPACE  SCALAR
+                  DATA {
+                  (0): "2024/07/01 16:42:45"
+                  }
+               }
+               ATTRIBUTE "Info" {
+                  DATATYPE  H5T_STRING {
+                     STRSIZE H5T_VARIABLE;
+                     STRPAD H5T_STR_NULLTERM;
+                     CSET H5T_CSET_UTF8;
+                     CTYPE H5T_C_S1;
+                  }
+                  DATASPACE  SCALAR
+                  DATA {
+                  (0): "Spline-Object"
+                  }
+               }
+               ATTRIBUTE "Package" {
+                  DATATYPE  H5T_STRING {
+                     STRSIZE H5T_VARIABLE;
+                     STRPAD H5T_STR_NULLTERM;
+                     CSET H5T_CSET_UTF8;
+                     CTYPE H5T_C_S1;
+                  }
+                  DATASPACE  SCALAR
+                  DATA {
+                  (0): "Spline Package by Claudius Haag"
+                  }
+               }
+               ATTRIBUTE "Version" {
+
+
+```
--- hdf5_graph/main.py
+++ hdf5_graph/main.py
@@ -1,29 +1,138 @@
 from neo4j import GraphDatabase
+import h5py
+from pathlib import Path
+
+# def add_dataset_to_neo4j(session, dataset):
+#     """
+#     Add a dataset to neo4j
+
+#     Args:
+#         session (_type_): _description_
+#         object (_type_): _description_
+#     """
+#     def _write(tx, dataset):
+#         result = tx.run("""
+#                 CREATE ()
+#                 """,
+#                 filter=name_filter)
+#         return list(result)
+#     session.execute_write(_write, dataset)
+
+def convert_value_to_cypher(dataset):
+    try:
+        if not dataset.shape:
+            return dataset[()]
+        else:
+            return None
+    except:
+        return None
+
+def visit(name, object):
+    """
+    Visit all items, check if it is dataset, if yes, put it into neo4j.
+
+    Args:
+        name (_type_): _description_
+        object (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    # decide if it is group or dataset
+    if isinstance(object, h5py.Group):
+        return None
+    elif isinstance(object, h5py.Dataset):
+        # Do sth!
+        # add_dataset_to_neo4j(session, object)
+        if name.split("/")[-1] in ["macrofail", "Spline_Interp", "datapoints", "dt", "ca", "ma", "jb"]:
+            result=driver.execute_query("""
+            MATCH (e:Experiment)
+            WHERE $parent CONTAINS e.hdf5_path
+            CREATE (e)-[:holds]->(dset:Dataset {name: $obj_name, hdf5_path:$hdf5_path, value:$value})
+            """,parent=object.parent.name,
+            obj_name=object.name.split("/")[-1],
+            hdf5_path=object.name,
+            value=convert_value_to_cypher(object))
+            summary = result.summary
+            print("Created {nodes_created} nodes in {time} ms.".format(
+                        nodes_created=summary.counters.nodes_created,
+                        time=summary.result_available_after
+                    ))
+        else:
+            pass
+        return None
+    else:
+        return 0
 
 if __name__ == "__main__":
     # URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
     URI = "neo4j://localhost"
     AUTH = ("neo4j", "neo4jadmin")
 
+    filepath = Path("data/CompleteData.h5")
+
     with GraphDatabase.driver(URI, auth=AUTH) as driver:
         driver.verify_connectivity()
 
-        # Get the name of all 42 year-olds
-        records, summary, keys = driver.execute_query(
-            """MATCH (a:Person {name:'Tom Hanks'})-[:ACTED_IN]->(m)<-[:ACTED_IN]-(coActors),
-                (coActors)-[:ACTED_IN]->(m2)<-[:ACTED_IN]-(cocoActors)
-            WHERE NOT (a)-[:ACTED_IN]->()<-[:ACTED_IN]-(cocoActors) AND a <> cocoActors
-            RETURN cocoActors.name AS Recommended, count(*) AS Strength ORDER BY Strength DESC""",
-            # age=42,
-            database_="neo4j",
-        )
+        # # Get the name of all 42 year-olds
+        # records, summary, keys = driver.execute_query(
+        #     """MATCH (a:Person {name:'Tom Hanks'})-[:ACTED_IN]->(m)<-[:ACTED_IN]-(coActors),
+        #         (coActors)-[:ACTED_IN]->(m2)<-[:ACTED_IN]-(cocoActors)
+        #     WHERE NOT (a)-[:ACTED_IN]->()<-[:ACTED_IN]-(cocoActors) AND a <> cocoActors
+        #     RETURN cocoActors.name AS Recommended, count(*) AS Strength ORDER BY Strength DESC""",
+        #     # age=42,
+        #     database_="neo4j",
+        # )
 
-        # Loop through results and do something with them
-        for person in records:
-            print(person)
+        # # Loop through results and do something with them
+        # for person in records:
+        #     print(person)
 
-        # Summary information
-        print("The query `{query}` returned {records_count} records in {time} ms.".format(
-            query=summary.query, records_count=len(records),
-            time=summary.result_available_after,
-        ))
+        # # Summary information
+        # print("The query `{query}` returned {records_count} records in {time} ms.".format(
+        #     query=summary.query, records_count=len(records),
+        #     time=summary.result_available_after,
+        # ))
+        # DELETE everything
+        driver.execute_query("""
+                        MATCH (n)
+                        DETACH DELETE n
+                        """)
+        data = []
+        with h5py.File(filepath, mode="r") as hdf:
+            summary=driver.execute_query("""
+                        CREATE (f:File {name: $obj_name, filepath:$path})
+                        """,
+                        obj_name=filepath.name,
+                        path=str(filepath))
+            for group in hdf.values():
+                # Create the experiment node
+                summary=driver.execute_query("""
+                        MATCH (f:File {name: $filename})
+                        CREATE (f)-[:holds]->(e:Experiment {name: $obj_name, hdf5_path:$hdf5_path})
+                        """,
+                        obj_name=group.name.split("/")[-1],
+                        hdf5_path=group.name,
+                        filename = filepath.name
+                        ).summary
+                print("Created {nodes_created} nodes in {time} ms.".format(
+                            nodes_created=summary.counters.nodes_created,
+                            time=summary.result_available_after
+                        ))
+                group.visititems(visit)
+                # feap_variables = {}
+                # for var, val in group["feap_variables"].items():
+                #     if val.dtype.kind != "f":
+                #         feap_variables[var] = val.asstr()[()]
+                #     else:
+                #         feap_variables[var] = val[()]
+                # for key in group["BF_curves"].keys():
+                #     if key.startswith("Curve_"):
+                #         intermed = {}
+                #         intermed["simulation"] = group.name
+                #         intermed["number_of_datapoints"] = max(group["BF_curves"][key]["datapoints"].shape)
+                #         intermed["curve_id"] = int(key.lstrip("Curve_"))
+                #         intermed.update(feap_variables)
+                #         if "ma" in feap_variables.keys() and "mb" in feap_variables.keys():
+                #             intermed.update({"mat_contrast": feap_variables["mb"]/feap_variables["ma"]})
+                #         data.append(intermed)
